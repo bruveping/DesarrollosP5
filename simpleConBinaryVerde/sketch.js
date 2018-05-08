0a5ae5c695b3b3d621caf70ff5b2b7cb866b076a
bruveping <bruveping@gmail.com>
2018-05-08 15:54:43 -0300

    esto es un homenaje al Opening de gost in the shell en P5

---
+++ simpleConBinaryVerde/sketch.js
@@ -0,0 +1,25 @@
+
+var elTamano;
+var divisor;
+var elAncho;
+
+function setup() {
+createCanvas(displayWidth,displayHeight);
+//fullScreen();
+divisor = 110;
+elTamano = height/divisor;
+elAncho = width/divisor;
+textSize(elTamano);
+fill(0,255,0);
+frameRate(12);
+  // put setup code here
+}
+
+function draw() {
+  background(0);
+  for(var i= 0; i<divisor; i++){
+      for(var j= 0; j<divisor; j++){
+    var elNumero = int(random(5))%2;
+text(elNumero,elAncho*j,elTamano*i);}}
+  // put drawing code here
+}
