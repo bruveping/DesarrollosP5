eac052e3b93935b2351ba9b9e06fadaf02ee8b70
bruvePing <26751664+bruveping@users.noreply.github.com>
2020-02-22 20:16:01 -0300

    Add files via upload

---
+++ dragAndDropP5/sketch.js
@@ -0,0 +1,31 @@
+let c;
+let elAudio;
+function setup() {
+    c = select('#SuelteAqui');
+    noCanvas();
+    background(200);
+    textAlign(CENTER);
+    text('Drag file', width / 2, height / 2);
+    //c.dragOver(dragOverCallback);
+    c.drop(recibeDocumento);
+    createP('hola');
+}
+
+// This function will be called whenever
+// a file is dragged over the canvas
+function dragOverCallback() {
+    c.style('background', '#aaa');
+}
+
+function recibeDocumento(file){
+    createP(file.size);
+    createP(file.name);
+    createP(file.data);
+    createP(file.type);
+
+    if(file.type=='audio'){
+    elAudio = new Audio(file.data);
+        elAudio.play(); 
+    }
+
+}
