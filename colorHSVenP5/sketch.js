e49eb6203195430230fad71feea12a4bf83ad482
bruvePing <26751664+bruveping@users.noreply.github.com>
2020-07-28 20:16:45 -0300

    Update sketch.js

--- colorHSVenP5/sketch.js
+++ colorHSVenP5/sketch.js
@@ -6,13 +6,16 @@ let elTitulo;
 let elSubTitu;
 let laTela;
 let elMargen;
+let imputTono;
+let imputSaturacion;
+let imputValor;
 
 function setup() {
     // put setup code here
-    
-        elMargen = '45px';
 
-        
+    elMargen = '45px';
+
+
     elTitulo = createElement('h1',"Tono Saturación Valor").style('font-family','sans-serif').style('margin-left',elMargen);
 
     elSubTitu =  createElement('h2',"Pagina Web para explorar HSB").style('font-family','sans-serif').style('margin-left',elMargen);
@@ -22,21 +25,27 @@ function setup() {
     background(0);
     createP("Tono").style('font-family','sans-serif').style('margin-left',elMargen);
     sliderTono = createSlider(0,360,0).style('margin-left',elMargen);
- 
+    imputTono = createInput().style('margin-left',elMargen);
+
     createP("Saturación").style('font-family','sans-serif').style('margin-left',elMargen);
-    
+
     sliderSaturacion = createSlider(0,100,0).style('margin-left',elMargen);
+    imputSaturacion = createInput().style('margin-left',elMargen);
 
     colorMode(HSB,360,100,100);
-    
+
     createP("Valor").style('font-family','sans-serif').style('margin-left',elMargen);
     sliderValor = createSlider(0,100,0).style('margin-left',elMargen);
- 
+    imputValor = createInput().style('margin-left',elMargen);
+
 
 }
 
 function draw() {
     // put drawing code here
     background(sliderTono.value(),sliderSaturacion.value(),sliderValor.value());
+    imputTono.value(sliderTono.value());
+    imputSaturacion.value(sliderSaturacion.value());
+    imputValor.value(sliderValor.value());
 
 }
