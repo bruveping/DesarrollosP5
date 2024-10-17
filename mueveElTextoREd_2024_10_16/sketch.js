f143200458975d31463db380b6fe6f65f58f93b5
bruvePing <26751664+bruveping@users.noreply.github.com>
2024-10-17 08:23:59 -0300

    Add files via upload

--- mueveElTextoREd_2024_10_16/sketch.js
+++ mueveElTextoREd_2024_10_16/sketch.js
@@ -19,7 +19,7 @@ function setup() {
   for (let i = 0; i < losStrings.length; i++) {
     losTextos.push(new lasLetras(random(width), random(height), random(-2, 2), random(-2, 2)));
   }
-  
+
   // Establecer el color y el grosor de las líneas
   stroke(200, 125, 125);
   strokeWeight(5);
@@ -59,12 +59,16 @@ class lasLetras {
   dibujaLaEllipse(texto) {
     let w = textWidth(texto) * 1.2; // Anchura de la elipse, 20% más grande que el texto
     let h = (textAscent() + textDescent()) * 1.5; // Altura de la elipse, 50% más grande que el texto
+    
     // Dibujar la elipse con el color asignado
     fill(this.color);
     ellipse(this.elPosX, this.elPosY, w, h); // Centrar verticalmente
+    
     // Dibujar el texto en blanco
     fill(255);
     text(texto, this.elPosX - textWidth(texto) / 2, this.elPosY + textAscent() / 3); // Ajuste horizontal
+    
+    // Mover el texto y la elipse
     this.mueveElTexto();
   }
   
